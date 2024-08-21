9eba53c96a4b09badd66d705f08cc8c0b7b2a378
YousefAhmed92 <125690867+YousefAhmed92@users.noreply.github.com>
2024-08-22 02:15:05 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,15 @@
+<?php
+$localhost="localhost";
+$username="root";
+$password="";
+$database="updated-case1";
 
+session_start();
+ob_start();
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+$connect=mysqli_connect($localhost,$username,$password,$database);
+?>
